e99ac20489b08e01049be95b5d2c538e3c4cc3e1
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-07-20 11:21:36 +0700

    feat: add listener service to send email

---
+++ src/listener.js
@@ -0,0 +1,25 @@
+const autoBind = require('auto-bind')
+
+class Listener {
+    constructor(playlistsService, mailSender) {
+        this._playlistsService = playlistsService,
+        this._mailSender = mailSender
+
+        autoBind(this)
+    }
+
+    async listen(message) {
+        try {
+            const { userId, playlistId, targetEmail } = JSON.parse(message.content.toString())
+            
+            const playlist = await this._playlistsService.getPlaylistDetails(playlistId, userId)
+            const result = await this._mailSender.sendEmail(targetEmail, JSON.stringify(playlist))
+
+            console.log(result)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+}
+
+module.exports = Listener
