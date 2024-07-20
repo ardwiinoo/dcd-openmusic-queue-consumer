7cb128768c0101162ac0f69be983746efc38e03a
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-07-21 05:45:20 +0700

    fix(creds): use correct env, adjust sendEmail function

--- src/MailSender.js
+++ src/MailSender.js
@@ -3,8 +3,8 @@ const { createTransport } = require('nodemailer')
 class MailSender {
     constructor() {
         this._transporter = createTransport({
-            host: process.env.MAIL_HOST,
-            port: process.env.MAIL_PORT,
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASSWORD
@@ -14,11 +14,11 @@ class MailSender {
 
     sendEmail(targetEmail, content) {
         const message = {
-            from: 'Open Music App',
+            from: 'Open Music App <no-reply@openmusicapp.com>',
             to: targetEmail,
             subject: 'Ekspor Playlist Songs',
             text: 'Terlampir hasil dari ekspor playlist songs',
-            attachment: [
+            attachments: [
                 {
                     filename: 'playlist_songs.json',
                     content
